9b2e8b9b5ddfbe785cd8ec93e1ea8071ce091e02
AI Central <167779704+AICentralSA@users.noreply.github.com>
2024-06-10 23:39:46 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { HomeScreen } from "./components/component/home-screen";
+
+export default function Home() {
+  return (
+    <main>
+      <HomeScreen />
+    </main>
+  );
+}
